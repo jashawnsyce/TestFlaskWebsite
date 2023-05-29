f94e204688f4c6dc585bf01291bfd2260bb89373
Jay Syce <jaysycework@gmail.com>
2023-05-29 15:05:12 -0400

    added base html templates

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p> Login </p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p> Logout </p>"
+    return render_template("logout.html")
 
 @auth.route('/signup')
 def signup():
-    return "<p> sign up </p>"
+    return render_template("signup.html")
